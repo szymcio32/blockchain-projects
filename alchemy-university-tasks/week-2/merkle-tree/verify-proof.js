e08d754ac8f1034e825f9e2c6f8d898bf0e0dbeb
szymon <wolnyszym@gmail.com>
2023-01-07 21:03:50 +0100

    Trie

--- alchemy-university-tasks/week-2/merkle-tree/verify-proof.js
+++ alchemy-university-tasks/week-2/merkle-tree/verify-proof.js
@@ -1,9 +1,7 @@
 function verifyProof(proof, node, root, concat) {
-    let currentProof = '';
     let itemToHash = node; 
     proof.forEach(elem => {
         itemToHash = elem['left'] === true ? concat(elem['data'], itemToHash) : concat(itemToHash, elem['data']);
-        currentProof += itemToHash;
     })
     return itemToHash === root;
 }
