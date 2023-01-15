6fcf61030df6a51d0c72f3f568b376b1a97342ba
szymon <wolnyszym@gmail.com>
2023-01-15 22:25:25 +0100

    week-3

---
+++ alchemy-university-tasks/week-3/donate.js
@@ -0,0 +1,24 @@
+const { utils, providers, Wallet } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+/**
+ * Donate at least 1 ether from the wallet to each charity
+ * @param   {string} a hex string private key for a wallet with 10 ETH
+ * @param   {array} an array of ethereum charity addresses
+ *
+ * @returns {Promise} a promise that resolves after all donations have been sent
+ */
+async function donate(privateKey, charities) {
+    const wallet = new Wallet(privateKey, provider);
+    for (charity of charities) {
+        await wallet.sendTransaction({
+            to: charity,
+            value: utils.parseEther('1')
+        })
+    }
+    // TODO: donate to charity!
+}
+
+module.exports = donate;
